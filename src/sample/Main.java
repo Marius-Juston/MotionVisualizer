d98d47cabcb8778b33eefcfe3e68f004e1d064b8
Allison <alcp2610@gmail.com>
2018-03-19 21:38:50 -0400

    changed title of application from hello world to motion visualizer
    y not

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("../field/fieldChooser.fxml"));
-		primaryStage.setTitle("Hello World");
+		primaryStage.setTitle("Motion Visualizer");
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 
